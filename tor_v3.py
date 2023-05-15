5be948d0dfb309b9bd9255762eee804a89872c86
Invizabel <46697708+Invizabel@users.noreply.github.com>
2023-05-15 13:35:35 -0500

    Update tor_v3.py

--- tor_v3.py
+++ tor_v3.py
@@ -141,11 +141,11 @@ def tor_v3_links():
         checksum = hashlib.sha3_256(b".onion checksum" + public + b"\x03").digest()[:2]
         result = "http://" + base64.b32encode(public + checksum + b"\x03").decode().lower() + ".onion"
         
-         with open("tor_links.txt", "r") as f:
-                for i in f:
-                    if result == i:
-                        repeat = True
-                        break
+        with open("tor_links.txt", "r") as f:
+            for i in f:
+                if result == i:
+                    repeat = True
+                    break
         
         if not repeat:
             with open("tor_links.txt", "a") as f:
